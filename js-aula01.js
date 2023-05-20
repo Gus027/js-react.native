644692734d48857c57d7e4cb91a3e91c03f92043
Gus027 <gustavogomesferreira2012@outlook.com>
2023-05-20 18:21:28 -0300

    feat: Aula 01

---
+++ js-aula01.js
@@ -0,0 +1,21 @@
+const pessoa = [
+    {
+    nome : 'Joao',
+    idade : 23,
+   },
+   {
+    nome : 'Alex',
+    idade : 43,
+   },
+   {
+    nome : 'Ricardo',
+    idade : 55,
+   }
+   ,
+   {
+    nome : 'Amanda',
+    idade : 12,
+   }
+]; 
+
+console.log(pessoa.nome);
